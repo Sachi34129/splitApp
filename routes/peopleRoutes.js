05e460d0dfa1822b1a6c2aadcbac51db0a0d6d88
Sachith <sachi34129@gmail.com>
2025-06-17 10:04:18 +0530

    update backend controllers

--- routes/peopleRoutes.js
+++ routes/peopleRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { getAllPeople } = require('../controllers/peopleController');
 
-router.get('/', getAllPeople);
+router.get('/people', getAllPeople);
 
 module.exports = router;
--- routes/settlementRoutes.js
+++ routes/settlementRoutes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { getSettlement } = require('../controllers/settlementController');
 
-router.get('/', getSettlement);
+router.get('/settlements', getSettlement);
 
 module.exports = router;
